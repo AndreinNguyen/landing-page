e3dcf67862a76542def1da6fc822841ed4f4606c
AndreinChef <nguyen.nguyen@savvycomsoftware.com>
2022-09-21 15:51:41 +0700

    Update social description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,27 @@ class MyDocument extends Document {
         <Head>
           <title>Savvy AI</title>
           <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon-32x32.png" />
+          <link rel="apple-touch-icon" href="/favicon-32x32.png" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=5, minimum-scale=1, viewport-fit=cover"
+          />
+          <meta
+            name="description"
+            content="Savvycom.ai is an ecosystem that seeks to promote and showcase innovative products made with blockchain and AI technologies. Our projects venture beyond the business sphere, aspiring to enact good changes on the societal level."
+          />
+          <meta name="theme-color" content="#14393c" />
+          <meta name="twitter:image" content="https://savvycom.ai/assets/images/savvybird.png" />
+          <meta
+            name="twitter:description"
+            content="Savvycom.ai is an ecosystem that seeks to promote and showcase innovative products made with blockchain and AI technologies. Our projects venture beyond the business sphere, aspiring to enact good changes on the societal level."
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:title"
+            content="😊 Savvycom.ai is an ecosystem that seeks to promote and showcase innovative products made with blockchain and AI technologies. Our projects venture beyond the business sphere, aspiring to enact good changes on the societal level."
+          />
         </Head>
         <body>
           <Main />
